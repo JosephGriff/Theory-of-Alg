55dc5cfa2f0672f9be49bc9a2fdd3d7eb5afcab8
JosephGriff <joeygriffith21@gmail.com>
2020-04-30 14:16:28 +0000

    refs/constants

--- finalMd5.c
+++ finalMd5.c
@@ -6,35 +6,24 @@
 #include <stdint.h>
 #include <inttypes.h>
 
-// Note: All variables are unsigned 32 bit and wrap modulo 2^32 when calculating
-var int s[64], K[64]
-var int i
+// ( precomputed table):
+//MD5
+//constans
 
-// s specifies the per-round shift amounts
-s[ 0..15] := { 7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22 }
-s[16..31] := { 5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20 }
-s[32..47] := { 4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23 }
-s[48..63] := { 6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21 }
-
-// Use binary integer part of the sines of integers (Radians) as constants:
-for i from 0 to 63 do
-    K[i] := floor(232 × abs (sin(i + 1)))
-end for
-// (Or just use the following precomputed table):
-K[ 0.. 3] := { 0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee }
-K[ 4.. 7] := { 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501 }
-K[ 8..11] := { 0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be }
-K[12..15] := { 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821 }
-K[16..19] := { 0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa }
-K[20..23] := { 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8 }
-K[24..27] := { 0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed }
-K[28..31] := { 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a }
-K[32..35] := { 0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c }
-K[36..39] := { 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70 }
-K[40..43] := { 0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05 }
-K[44..47] := { 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665 }
-K[48..51] := { 0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039 }
-K[52..55] := { 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1 }
-K[56..59] := { 0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1 }
-K[60..63] := { 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391 }
+const uint32_t K[64] = {0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+	              0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+		      0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+		      0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+		      0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+		      0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+		      0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+		      0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+		      0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+		      0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+		      0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
+		      0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+		      0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+		      0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+		      0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+		      0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391};
 
