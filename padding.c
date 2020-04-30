96dd6ae546c3fce17e004d8790b7e2c97db69611
JosephGriff <joeygriffith21@gmail.com>
2020-04-30 08:32:02 +0000

    padding

--- padding.c
+++ padding.c
@@ -4,6 +4,18 @@
 #include <stdio.h>
 #include <inttypes.h>
 
+uint64_t no_zeros_bytes(uint64_t no_bits) {
+    uint64_t result = 512 - (no_bits % 512ULL);
+
+    if (result < 65) {
+        result += 512;
+    }
+
+    result -= 72;
+
+    return (result / 8ULL);
+}
+
 int main(int argc, char *argv[]) {
     
     if (argc != 2) {
@@ -17,6 +29,19 @@ int main(int argc, char *argv[]) {
         printf("Error: couldn't open file %s. \n", argv[1]);
         return 1;
     }
+
+    uint64_t noBits;
+   
+    uint8_t i;
+
+    for (noBits = 0, i = 0; fread(&i, 1, 1, inFile) == 1; noBits += 8) {
+        printf("%02" PRIx8 " " ,i );
+    }
+
+    printf("%02" PRIx8, 0x80); // Bits: 1000 0000
+
+    printf("%016" PRIx64 "\n", noBits);
+
 
     fclose(inFile);
     return 0;
