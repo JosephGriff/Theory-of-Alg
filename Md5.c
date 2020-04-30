0d31586303cbe08175ecc35f3108a0419ee64082
JosephGriff <joeygriffith21@gmail.com>
2020-04-30 10:20:57 +0000

    merging nearly done

--- Md5.c
+++ Md5.c
@@ -78,7 +78,10 @@ uint32_t sig1(uint32_t x) {
     uint8_t eight[64]; // 8 * 64 = 512
 }; 
 
+enum flag {READ, PAD0, PAD1, FINISH};
+
 uint64_t no_zeros_bytes(uint64_t no_bits) {
+
     uint64_t result = 512 - (no_bits % 512ULL);
 
     if (result < 65) {
@@ -90,23 +93,25 @@ uint64_t no_zeros_bytes(uint64_t no_bits) {
     return (result / 8ULL);
 }
 
-int nextblock(union block *M, FILE *infile) {
+int nextblock(union block *M, FILE *infile, uint64_t *nobits, enum flag *status ) {
 
-    uint64_t noBits;
     uint8_t i;    
 	
-    for (noBits = 0, i = 0; fread(&M.eight[i], 1, 1, inFile) == 1; noBits += 8) {
+    for (*noBits = 0, i = 0; fread(&M.eight[i], 1, 1, inFile) == 1; *noBits += 8) {
         printf("%02" PRIx8, M.eight[i]);
     }
 
     printf("%02" PRIx8, 0x80); // Bits: 1000 0000
 
-    for (uint64_t i = (no_zeros_bytes(noBits)); i > 0; i--) {
+    for (uint64_t i = (no_zeros_bytes(*noBits)); i > 0; i--) {
         printf("%02" PRIx8, 0x00);
     }
 
-    printf("%016" PRIx64 "\n", noBits);
+    printf("%016" PRIx64 "\n", *noBits);
 
+void nexthash(union block *M, uint32_t *H){
+    
+}
 
 int main(int argc, char *argv[]) {
     
@@ -131,10 +136,12 @@ int main(int argc, char *argv[]) {
 
     //the current padded message block
     union bock M;
+    uint64_t nobits = 0;
+    enum flag status = READ;
 
-    while (nextblock(&M, infile)) {
+    while (nextblock(&M, infile, nobits, status)) {
       // Calculate the next hash value.
-      H =  nexthash(M, H);
+      H =  nexthash(&M,&H);
     }
  
     for (int i = 0; i < 8; i++)
