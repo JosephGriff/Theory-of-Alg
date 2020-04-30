5776979f5e5dd937904aefeacdcf2c77d56a2d66
JosephGriff <joeygriffith21@gmail.com>
2020-04-30 08:37:25 +0000

    padding

--- padding.c
+++ padding.c
@@ -35,10 +35,14 @@ int main(int argc, char *argv[]) {
     uint8_t i;
 
     for (noBits = 0, i = 0; fread(&i, 1, 1, inFile) == 1; noBits += 8) {
-        printf("%02" PRIx8 " " ,i );
+        printf("%02" PRIx8 ,i );
     }
 
-    printf("%02" PRIx8, 0x80); // Bits: 1000 0000
+    printf("%02" PRIx8, 0x80); // Bits: 1000 000
+    
+    for (uint64_t i = (no_zeros_bytes(noBits)); i > 0; i--) {
+        printf("%02" PRIx8, 0x00);
+    }0
 
     printf("%016" PRIx64 "\n", noBits);
 
