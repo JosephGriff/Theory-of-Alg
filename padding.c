aa43496cbe31940be52228fba297f7fd811f831a
JosephGriff <joeygriffith21@gmail.com>
2020-04-30 09:11:25 +0000

    padding/unions

--- padding.c
+++ padding.c
@@ -4,6 +4,12 @@
 #include <stdio.h>
 #include <inttypes.h>
 
+union block {
+    uint64_t sixFour[8]; // 64 * 8 = 512
+    uint32_t threeTwo[16]; // 32 * 16 = 512
+    uint8_t eight[64]; // 8 * 64 = 512
+}; 
+
 uint64_t no_zeros_bytes(uint64_t no_bits) {
     uint64_t result = 512 - (no_bits % 512ULL);
 
@@ -31,18 +37,18 @@ int main(int argc, char *argv[]) {
     }
 
     uint64_t noBits;
-   
+    union block M;
     uint8_t i;
 
-    for (noBits = 0, i = 0; fread(&i, 1, 1, inFile) == 1; noBits += 8) {
-        printf("%02" PRIx8 ,i );
+    for (noBits = 0, i = 0; fread(&M.eight[i], 1, 1, inFile) == 1; noBits += 8) {
+        printf("%02" PRIx8, M.eight[i]);
     }
 
     printf("%02" PRIx8, 0x80); // Bits: 1000 000
     
     for (uint64_t i = (no_zeros_bytes(noBits)); i > 0; i--) {
         printf("%02" PRIx8, 0x00);
-    }0
+    }
 
     printf("%016" PRIx64 "\n", noBits);
 
